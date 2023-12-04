029713cbe76058bed567515b0a05481ceaa06014
Piotr Bukowiec <piotrbukowiec@DESKTOP-M4CBP.local>
2023-12-04 15:55:00 +0100

    Removed class-validator and class-transformer

--- src/translator/dto/langReqBody.dto.ts
+++ src/translator/dto/langReqBody.dto.ts
@@ -1,13 +1,6 @@
-import { IsEnum, IsString, Length } from 'class-validator';
 import { SourceLanguageCode, TargetLanguageCode } from 'deepl-node';
-import { SourceLangCode } from '../enum/sourceLangCode.enum';
-import { TargetLangCode } from '../enum/targetLangCode.enum';
 export class LangReqBody {
-  @IsEnum(SourceLangCode)
-  @Length(2, 4)
   readonly sourceLang: SourceLanguageCode;
-  @IsString() @Length(1, 200) text: string;
-  @IsEnum(TargetLangCode)
-  @Length(2, 4)
+  readonly text: string;
   readonly targetLang: TargetLanguageCode;
 }
