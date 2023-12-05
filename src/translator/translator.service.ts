f67bd5dd5a1bc5c762af90325c4c852e896b685a
Piotr Bukowiec <piotrbukowiec@DESKTOP-M4CBP.local>
2023-12-05 20:16:34 +0100

    No DTO validation, everything works

--- src/translator/translator.service.ts
+++ src/translator/translator.service.ts
@@ -1,14 +1,11 @@
-import { Injectable, Res } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Translator, SourceLanguageCode, TargetLanguageCode } from 'deepl-node';
-import { Response } from 'express';
 @Injectable()
 export class TranslatorService {
   async translate(
     sourceLang: SourceLanguageCode,
     text: string,
     targetLang: TargetLanguageCode,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    @Res() res: Response,
   ): Promise<string> {
     const translator: Translator = new Translator(process.env.AUTH_KEY);
     return JSON.stringify(
@@ -16,4 +13,3 @@ export class TranslatorService {
     );
   }
 }
-//TODO walidacja dto
