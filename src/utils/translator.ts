736cf92e42b92b3587ed45fe97a9d09a4de59607
Piotr Bukowiec <piotrbukowiec@desktop-m4cbp.home>
2023-12-17 17:46:15 +0100

    Fixed translate function

--- src/utils/translator.ts
+++ src/utils/translator.ts
@@ -3,4 +3,4 @@ import { DEEPL_AUTH_KEY as authKey } from '../constants';
 const translator = new Translator(String(authKey));
 const translate = async (text: string, targetLang: TargetLanguageCode) =>
 	(await translator.translateText(text, null, targetLang)).text;
-export { translate };
+export { translate, translator };
